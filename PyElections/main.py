4a667d2ae905b2da360079c8f290145537cf2f16
tjjaramillo <58151139+tjjaramillo@users.noreply.github.com>
2020-02-22 16:33:32 -0600

    needs text file

--- PyElections/main.py
+++ PyElections/main.py
@@ -27,27 +27,44 @@ with open(Election_Data, newline="") as csvfile:
 
     for row in csvreader:
 
+        # Start loop with adding a vote to the total vote count
+
         Total_Vote_Count += 1
+
+        # If candidate name is not in the existing list of candidates, add them to the list and put a 
+        # vote with their name
 
         if row[0] not in Candidates:
             Candidates.append(row[0])
             Vote_Count.append(1)
 
+        #If candidate name is in the existing list of candidates, add a vote to their name with their existing index
+
         else:
             candidate_index = Candidates.index(row[0])
             Vote_Count[candidate_index] += 1
 
+    # Take vote percentage of each candidate from the total amount of votes
     
-
     for vote in Vote_Count:
         percentage = (vote/Total_Vote_Count)
         percentage = round(percentage)
         percentage = format(percentage, ".2%")
         Vote_Percentage.append(percentage)
 
+    # Capture the winning candidate with the highest vote count
+
     Winning_Candidate = max(Vote_Count)
     index = Vote_Count.index(Winning_Candidate)
     Winning_Candidate = Candidates[index]
 
-print(str(Total_Vote_Count))
-
+# Printed election results
+print("Election Results")
+print("-----------------------------")
+print("Total Votes: " + str(Total_Vote_Count))
+print("----------------------------")
+for i in range(len(Candidates)):
+    print(f"{Candidates[i]}: {str(Vote_Percentage[i])})")
+print("----------------------------")
+print(f"Winner: {Winning_Candidate}")
+print("----------------------------")
